eba7bba02a5d57f9014d24b90faae6ed4947f5eb
pureinsomnia <59368489+pureinsomnia@users.noreply.github.com>
2020-01-30 22:44:46 +0200

    more changes to db

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ const image = require ('./controllers/image');
 const db = knex ({
         client: 'pg',
         connection: {
-          host : 'postgresql-defined-62733',
-          user : 'postgres',
-          password : 'denisa',
-          database : 'smart-brain'
+          host : process.env.DATABASE_URL,
+          ssl:true
+          
         }
       
 })
